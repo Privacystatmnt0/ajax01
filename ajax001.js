c44a38bc555ce9c632f853fe9c847cc14fcd8de9
Privacystatmnt0 <118718627+Privacystatmnt0@users.noreply.github.com>
2022-11-21 15:46:50 +0800

    Create ajax001.js

---
+++ ajax001.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+$('<div><a href="https://rtpgrabwin.live/" target="_blank"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjmntoUdYX_FlTEPfj5wDgZW8kyWvQBg0iXWEjUZAwM9GoKQolAhHnidyfo9SUlA6LtI_WNbC9wlnpuLqScABxi1wE2o7gBOIpViMkyO3NJM0ooq1KQxafhF15sQmTVk0fK6RwY1E0wsps63IpcPJi4Db9mM21OSIK_FK4FM49r_ZwmANaNW62wd7VITQ/s320/rtp-slot-21-11-2022%20(1).gif" alt="RTP Grabwin" style="position: fixed; z-index: 1000; width: 80px; height: 80px; bottom: 5rem; left: 1rem;"></a></div>').insertAfter($(".fixed-footer"));
+});
